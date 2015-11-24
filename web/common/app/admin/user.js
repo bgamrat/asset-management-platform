4212531c3385cf2d292d89458ef49310c05950f4
bgamrat <betsy@mugo.ca>
2015-11-23 20:02:28 -0500

    Added dialog box UI for editing, changed to use tundra theme, style updates, initial remove UI

--- web/common/app/admin/user.js
+++ web/common/app/admin/user.js
@@ -5,18 +5,49 @@ require([
     "dojo/dom-attr",
     "dojo/json",
     "dijit/registry",
+    "dijit/form/TextBox",
+    "dijit/form/CheckBox",
+    "dijit/form/Button",
+    "dijit/Dialog",
     'dstore/RequestMemory',
     'dgrid/OnDemandGrid',
     "dgrid/Selection",
+    'dgrid/Editor',
+    "dojo/query",
     "dojo/domReady!"
-], function (declare, dom, xhr, domAttr, json, registry, RequestMemory, OnDemandGrid, Selection) {
+], function (declare, dom, xhr, domAttr, json, registry, TextBox, CheckBox, 
+        Button, Dialog, RequestMemory, OnDemandGrid, Selection, Editor) {
 
-    var grid = new (declare([OnDemandGrid, Selection]))({
+    var emailInput = new TextBox({}, "user_email");
+    var usernameInput = new TextBox({}, "user_username");
+    var enabledCheckBox = new CheckBox({}, "user_enabled");
+    
+    var saveBtn = new Button({
+        label: "Save"
+    }, 'save-btn');
+    saveBtn.startup();
+
+    var userViewDialog = new Dialog({
+        title: "View"
+    }, "user-view-dialog");
+    userViewDialog.on("cancel", function (event) {
+        grid.clearSelection();
+    });
+    userViewDialog.startup();
+
+    var grid = new (declare([OnDemandGrid, Selection, Editor]))({
         collection: new RequestMemory({target: '/api/admin/user/list'}),
         columns: {
             username: 'Username',
             email: 'Email',
-            enabled: 'Enabled'
+            enabled: 'Enabled',
+            remove: {
+                width: "10%",
+                editor: 'checkbox',
+                label: 'Remove',
+                autoSave: true,
+                sortable: false
+            }
         },
         selectionMode: "single"
     }, 'grid');
@@ -24,13 +55,11 @@ require([
     grid.startup();
 
     grid.on("dgrid-select", function (event) {
-        console.log('select');
-        console.log(event);
         var username = event.rows[0].data.username;
         xhr("/api/admin/user/" + username, {
             handleAs: "json"
         }).then(function (data) {
-            console.log(data);
+            userViewDialog.show();
             domAttr.set("user_username", "value", data.username);
             domAttr.set("user_email", "value", data.email);
             domAttr.set("user_enabled", "checked", data.enabled === true);
