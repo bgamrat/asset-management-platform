bdacf98dbf561e4db418a41e7f7eab34e037bfc0
bgamrat <betsy@mugo.ca>
2015-11-29 18:27:05 -0500

    Grid row selection controls, remove checkbox management, widget destruction

--- web/common/app/admin/user.js
+++ web/common/app/admin/user.js
@@ -1,11 +1,12 @@
 require([
     "dojo/_base/declare",
     "dojo/dom",
+    "dojo/on",
     "dojo/request/xhr",
     "dojo/dom-attr",
     "dojo/json",
     "dijit/registry",
-    "dijit/form/TextBox",
+    "dijit/form/ValidationTextBox",
     "dijit/form/CheckBox",
     "dijit/form/Button",
     "dijit/Dialog",
@@ -13,20 +14,19 @@ require([
     'dgrid/OnDemandGrid',
     "dgrid/Selection",
     'dgrid/Editor',
+    "dojo/aspect",
     "dojo/query",
     "dojo/domReady!"
-], function (declare, dom, xhr, domAttr, json, registry, TextBox, CheckBox, 
-        Button, Dialog, RequestMemory, OnDemandGrid, Selection, Editor) {
+], function (declare, dom, on, xhr, domAttr, json, registry, ValidationTextBox, CheckBox,
+        Button, Dialog, RequestMemory, OnDemandGrid, Selection, Editor, aspect, query) {
 
-    var emailInput = new TextBox({}, "user_email");
-    var usernameInput = new TextBox({}, "user_username");
+    var emailInput = new ValidationTextBox({}, "user_email");
+    var usernameInput = new ValidationTextBox({}, "user_username");
     var enabledCheckBox = new CheckBox({}, "user_enabled");
-    
     var saveBtn = new Button({
         label: "Save"
     }, 'save-btn');
     saveBtn.startup();
-
     var userViewDialog = new Dialog({
         title: "View"
     }, "user-view-dialog");
@@ -34,37 +34,64 @@ require([
         grid.clearSelection();
     });
     userViewDialog.startup();
-
     var grid = new (declare([OnDemandGrid, Selection, Editor]))({
         collection: new RequestMemory({target: '/api/admin/user/list'}),
         columns: {
-            username: 'Username',
-            email: 'Email',
-            enabled: 'Enabled',
+            username: {
+                label: 'Username'
+            },
+            email: {
+                label: 'Email'
+            },
+            enabled: {
+                label: 'Enabled'
+            },
             remove: {
                 width: "10%",
-                editor: 'checkbox',
+                editor: CheckBox,
                 label: 'Remove',
-                autoSave: true,
                 sortable: false
             }
         },
-        selectionMode: "single"
+        selectionMode: "none"
     }, 'grid');
 
+
     grid.startup();
-
-    grid.on("dgrid-select", function (event) {
-        var username = event.rows[0].data.username;
-        xhr("/api/admin/user/" + username, {
-            handleAs: "json"
-        }).then(function (data) {
-            userViewDialog.show();
-            domAttr.set("user_username", "value", data.username);
-            domAttr.set("user_email", "value", data.email);
-            domAttr.set("user_enabled", "checked", data.enabled === true);
-        });
-
+    grid.on(".dgrid-row:click", function (event) {
+        console.log(event);
+        var options = {handleAs: "json"};
+        var row = grid.row(event);
+        var cell = grid.cell(event);
+        var username = row.data.username;
+        if( cell.column.field !== "remove" ) {
+            if( typeof grid.selection[0] !== "undefined" ) {
+                grid.clearSelection();
+            }
+            grid.select(row);
+            options.method = "GET";
+            xhr("/api/admin/user/" + username, options).then(function (data) {
+                userViewDialog.show();
+                domAttr.set("user_username", "value", data.username);
+                domAttr.set("user_email", "value", data.email);
+                domAttr.set("user_enabled", "checked", data.enabled === true);
+            });
+        } else {
+            if( confirm("Are you sure?") ) {
+                options.method = "DELETE";
+                xhr("/api/admin/user/" + username, options).then(function (data) {
+                    grid.removeRow(row);
+                });
+            }
+        }
     });
 
+    aspect.before(grid, "removeRow", function (rowElement) {
+        // Destroy the checkbox widgets
+        var cellElement = grid.cell(rowElement, "remove").element,
+                widget = (cellElement.contents || cellElement).widget;
+        if( widget ) {
+            widget.destroyRecursive();
+        }
+    });
 });
