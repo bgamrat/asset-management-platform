4e61b1ce5d532e7c81f352cf0e9829855d838153
bgamrat <betsy@mugo.ca>
2015-11-30 20:38:20 -0500

    Widget cleanup, added check-all toggle on remove column, added New and Remove buttons

--- web/common/app/admin/user.js
+++ web/common/app/admin/user.js
@@ -1,10 +1,13 @@
 require([
     "dojo/_base/declare",
     "dojo/dom",
+    "dojo/dom-attr",
+    "dojo/dom-construct",
     "dojo/on",
     "dojo/request/xhr",
-    "dojo/dom-attr",
     "dojo/json",
+    "dojo/aspect",
+    "dojo/query",
     "dijit/registry",
     "dijit/form/ValidationTextBox",
     "dijit/form/CheckBox",
@@ -14,26 +17,42 @@ require([
     'dgrid/OnDemandGrid',
     "dgrid/Selection",
     'dgrid/Editor',
-    "dojo/aspect",
-    "dojo/query",
     "dojo/domReady!"
-], function (declare, dom, on, xhr, domAttr, json, registry, ValidationTextBox, CheckBox,
-        Button, Dialog, RequestMemory, OnDemandGrid, Selection, Editor, aspect, query) {
+], function (declare, dom, domAttr, domConstruct, on, xhr, json, aspect, query,
+        registry, ValidationTextBox, CheckBox, Button, Dialog,
+        RequestMemory, OnDemandGrid, Selection, Editor) {
+
+    var newBtn = new Button({
+        label: "New"
+    }, 'new-btn');
+    newBtn.startup();
+    var removeBtn = new Button({
+        label: "Remove"
+    }, 'remove-btn');
+    removeBtn.startup();
 
     var emailInput = new ValidationTextBox({}, "user_email");
+    emailInput.startup();
+
     var usernameInput = new ValidationTextBox({}, "user_username");
+    usernameInput.startup();
+
     var enabledCheckBox = new CheckBox({}, "user_enabled");
+    enabledCheckBox.startup();
+
     var saveBtn = new Button({
         label: "Save"
     }, 'save-btn');
     saveBtn.startup();
+
     var userViewDialog = new Dialog({
         title: "View"
     }, "user-view-dialog");
+    userViewDialog.startup();
     userViewDialog.on("cancel", function (event) {
         grid.clearSelection();
     });
-    userViewDialog.startup();
+
     var grid = new (declare([OnDemandGrid, Selection, Editor]))({
         collection: new RequestMemory({target: '/api/admin/user/list'}),
         columns: {
@@ -50,14 +69,18 @@ require([
                 width: "10%",
                 editor: CheckBox,
                 label: 'Remove',
-                sortable: false
+                sortable: false,
+                className: "remove-cb",
+                renderHeaderCell: function (node) {
+                    var inp = domConstruct.create("input", {id: "cb-all", type: "checkbox"});
+                    return inp;
+                }
             }
         },
         selectionMode: "none"
     }, 'grid');
-
-
     grid.startup();
+
     grid.on(".dgrid-row:click", function (event) {
         console.log(event);
         var options = {handleAs: "json"};
@@ -86,6 +109,14 @@ require([
         }
     });
 
+    var cbAll = new CheckBox({}, "cb-all");
+    cbAll.startup();
+    cbAll.on("click", function (event) {
+        query(".dgrid-row .remove-cb input").forEach(function (node) {
+            registry.byId(node.id).set("checked", !node.checked);
+        });
+    });
+
     aspect.before(grid, "removeRow", function (rowElement) {
         // Destroy the checkbox widgets
         var cellElement = grid.cell(rowElement, "remove").element,
@@ -94,4 +125,6 @@ require([
             widget.destroyRecursive();
         }
     });
+
+
 });
