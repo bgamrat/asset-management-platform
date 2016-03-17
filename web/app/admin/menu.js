652e66ccc28d3505781bbd3012574acf06ae9c58
bgamrat <betsy.gamrat@wirehopper.com>
2016-03-16 23:18:51 -0400

    Initial integration of Dijit menu

--- web/app/admin/menu.js
+++ web/app/admin/menu.js
@@ -1,42 +1,68 @@
-require([
+define([
+    "dojo/_base/declare",
+    "dojo/dom",
+    "dojo/dom-attr",
+    "dojo/dom-construct",
+    "dojo/on",
+    "dojo/request/xhr",
+    "dojo/json",
+    "dojo/aspect",
+    "dojo/query",
+    "dijit/registry",
     "dijit/MenuBar",
+    "dijit/MenuBarItem",
     "dijit/PopupMenuBarItem",
     "dijit/MenuItem",
     "dijit/DropDownMenu",
+    "dijit/form/Button",
+    "dijit/Dialog",
+    "app/lib/common",
+    "dojo/i18n!app/nls/core",
+    "dojo/NodeList-traverse",
     "dojo/domReady!"
-], function(MenuBar, PopupMenuBarItem, MenuItem, DropDownMenu){
-    var menuBar = new MenuBar({});
+], function (declare, dom, domAttr, domConstruct, on, xhr, json, aspect, query,
+        registry, MenuBar, MenuBarItem, PopupMenuBarItem, MenuItem, DropDownMenu, Dialog,
+        lib, libGrid, core) {
 
-    var subMenu = new DropDownMenu({});
-    pSubMenu.addChild(new MenuItem({
-        label: "File item #1"
-    }));
-    pSubMenu.addChild(new MenuItem({
-        label: "File item #2"
-    }));
-    pMenuBar.addChild(new PopupMenuBarItem({
-        label: "File",
-        popup: pSubMenu
-    }));
+    function run() {
+        var menuBar = new MenuBar({}, "admin-top-menu");
 
-    var pSubMenu2 = new DropDownMenu({});
-    pSubMenu2.addChild(new MenuItem({
-        label: "Cut",
-        iconClass: "dijitEditorIcon dijitEditorIconCut"
-    }));
-    pSubMenu2.addChild(new MenuItem({
-        label: "Copy",
-        iconClass: "dijitEditorIcon dijitEditorIconCopy"
-    }));
-    pSubMenu2.addChild(new MenuItem({
-        label: "Paste",
-        iconClass: "dijitEditorIcon dijitEditorIconPaste"
-    }));
-    pMenuBar.addChild(new PopupMenuBarItem({
-        label: "Edit",
-        popup: pSubMenu2
-    }));
+        function createMenuItem(widget,parent) {
+            var children, node, item, i, nextLabel = "";
+            console.log(widget);
+            console.log(parent);
+            children = query(parent).children();
+            for( i = 0; i < children.length; i++ ) {
+                node = children[i];
+                console.log(node);
+                switch( node.tagName ) {
+                    case "A":
+                        item = new MenuBarItem({label: node.textContent.trim()}, node);
+                        widget.addChild(item);
+                        break;
+                    case "UL":
+                        item = new DropDownMenu();
+                        widget.addChild(new PopupMenuBarItem({label: nextLabel, popup:item}));
+                        widget = item;    
+                    case "LI":
+                        createMenuItem(widget,node);     
+                        break;
+                    case "SPAN":
+                        nextLabel = node.textContent.trim();
+                        break;
+                }
+            }
+        }
 
-    pMenuBar.placeAt("wrapper");
-    pMenuBar.startup();
+        var menuElements = query("#admin-top-menu ul");
+        if( menuElements.length > 0 ) {
+            createMenuItem(menuBar,menuElements[0]);
+            domConstruct.destroy(menuElements[0]);
+        }
+
+        menuBar.startup();
+    }
+    return {
+        run: run
+    };
 });
