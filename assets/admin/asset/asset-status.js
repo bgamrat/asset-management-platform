0641091a975012cabb46f283fe5474bed7fe5ed0
Betsy Gamrat <betsy.gamrat@wirehopper.com>
2018-10-24 21:01:02 -0400

    A big vue mess.  Learning.

---
+++ assets/admin/asset/asset-status.js
@@ -0,0 +1,7 @@
+import AddRow from '../../components/Admin/AssetStatus.vue'
+import './asset-status.scss'
+import Vue from 'vue'
+new Vue({
+    el: '#addrow',
+    components: {addrow: AddRow}
+});
